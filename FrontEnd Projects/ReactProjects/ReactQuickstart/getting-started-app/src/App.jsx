333d9dcd35af90372c7385da8302f98c62d05b85
preethamdheeraj <chpreetham45@gmail.com>
2024-01-25 21:47:46 +0530

    MOD: made changes to getting started project

--- FrontEnd Projects/ReactProjects/ReactQuickstart/getting-started-app/src/App.jsx
+++ FrontEnd Projects/ReactProjects/ReactQuickstart/getting-started-app/src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function MyButton() {
     return <button>This is a simple button</button>;
@@ -29,6 +30,51 @@ const products = [
     { title: 'Apple', isFruit: true, id: 3 },
 ];
 
+function ShoppingList() {
+    const listItems = products.map((product) => (
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen',
+            }}>
+            {product.title}
+        </li>
+    ));
+
+    return <ul>{listItems}</ul>;
+}
+
+/* responding to events  */
+function MyButtonTwo() {
+    function handleClick() {
+        alert(
+            'this will open the alert box in the console and will show this message'
+        );
+    }
+
+    return (
+        <>
+            <p>
+                click on the below button to see the alert box functionality
+                work
+            </p>
+            <button onClick={handleClick}>Click me</button>
+        </>
+    );
+}
+
+/* updating the screen */
+function MyButtonThree() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return <button onClick={handleClick}>Clicked {count} times</button>;
+}
+
+/* using hooks now we will be designing counters that update together */
 function App() {
     return (
         <div className="App">
@@ -48,7 +94,7 @@ function App() {
                     rel="noopener noreferrer">
                     Learn React
                 </a>
-                <h2>welcome to this very simple react application</h2>
+                <h2>Welcome to this very simple react application</h2>
                 <MyButton />
                 <h3>{user.name}</h3>
                 <img
@@ -60,6 +106,13 @@ function App() {
                         height: user.imageSize,
                     }}
                 />
+                <ShoppingList />
+                <MyButtonTwo />
+                <p>
+                    click on the below button to see a count of how many number
+                    of times you have clicked onthe button
+                </p>
+                <MyButtonThree />
             </header>
         </div>
     );
